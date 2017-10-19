9adb1c0fe31dd4f47bc81f21ea43f1ee7787cd6f
diskmanti <diskmanti@yahoo.co.uk>
2017-10-19 21:36:11 +0200

    started working on pratical exercises

---
+++ query.sql
@@ -0,0 +1,18 @@
+﻿SELECT * FROM pop_places LIMIT 10
+
+SELECT name, pop_max, pop_min FROM pop_places WHERE sov0name = 'Uruguay'
+
+SELECT * FROM geometry_columns;
+
+SELECT name, ST_GeometryType(geom), ST_NDims(geom), ST_SRID(geom)  FROM pop_places;
+
+SELECT name, ST_X(geom), ST_Y(geom), ST_SRID(geom), ST_AsText(geom) FROM pop_places WHERE name = 'Trinidad'
+
+SELECT ST_Area(geom) From admin Where name='Angola'
+
+SELECT SUM(ST_Length(geom)) FROM roads
+
+---
+
+SELECT name
+FROM 
